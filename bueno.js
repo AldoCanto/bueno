72444ed9442a2c617f2e89d44b421ac4bb338a35
Aldo Canto <60444424+AldoCanto@users.noreply.github.com>
2020-04-06 02:17:35 -0500

    se agrega javascript

---
+++ bueno.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+  $('.carrusel').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+});
